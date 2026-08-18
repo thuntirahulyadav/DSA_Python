5229778c439f1dad8f0eb75ede773d9f9f5fa048
thuntirahulyadav <thuntirahulyadav@gmail.com>
2026-08-18 22:43:34 +0530

    Initial commit

---
+++ Arrays/Leader_in_Array.py
@@ -0,0 +1,11 @@
+def leaders_of_array(arr)->list:
+    leaders =[]
+    max_right= float('-inf')
+    for i in range(len(arr)-1,-1,-1):
+        if arr[i] > max_right:
+            leaders.append(arr[i])
+            max_right = arr[i]
+    leaders.reverse()
+    return leaders        
+arr = [10,22,12,3,0,6]    
+print(leaders_of_array(arr))
